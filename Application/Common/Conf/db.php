3029e04d16e735700f413f955488fc8a2eb4d692
shuang.liu <shuang.liu@missionpublic.com>
2020-08-26 09:53:00 +0800

    修改数据库配置

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'               =>  'mysql',     // 数据库类型
-	'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-	'DB_NAME'               =>  '522e10khcs',       // 数据库名
+	'DB_HOST'               =>  '59.111.97.208', // 服务器地址
+	'DB_NAME'               =>  'book_platform',       // 数据库名
 	'DB_USER'               =>  'root',      // 用户名
-	'DB_PWD'                =>  '854hhLI++n159',      // 密码
-	'DB_PORT'               =>  '3306',      // 端口
+	'DB_PWD'                =>  'MissionPublic2020!',      // 密码
+	'DB_PORT'               =>  '36306',      // 端口
 	'DB_PREFIX'             =>  'vv_',    	 // 数据库表前缀
 );
